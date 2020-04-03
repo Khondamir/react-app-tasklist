651f00a76d0a0983a91a8345b888d38f13d2d501
Khondamir <khondamirbek@gmail.com>
2020-04-04 01:08:46 +0500

    Updates

---
+++ precache-manifest.b7c8d8d6099c4df3a8219024c08fea7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba234312bac53ab5d09b9325caecc5e6",
+    "url": "/react-app-tasklist/index.html"
+  },
+  {
+    "revision": "9e69e741d8c848bf75bd",
+    "url": "/react-app-tasklist/static/css/main.e60dc301.chunk.css"
+  },
+  {
+    "revision": "387e0aa621b77a4e69b8",
+    "url": "/react-app-tasklist/static/js/2.53f650ec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-app-tasklist/static/js/2.53f650ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e69e741d8c848bf75bd",
+    "url": "/react-app-tasklist/static/js/main.343c029a.chunk.js"
+  },
+  {
+    "revision": "aec310fe586ee5882692",
+    "url": "/react-app-tasklist/static/js/runtime-main.54811afc.js"
+  }
+]);
